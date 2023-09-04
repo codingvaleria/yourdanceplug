bfd52fa6ede6fe97c4de53e6f4a4df0df4704800
codingvaleria <codingvaleria@gmail.com>
2023-09-04 14:25:32 +0300

    Define the event model constructor

--- danceplug.js
+++ danceplug.js
@@ -1,8 +1,23 @@
-class User{
-    constructor(obj) {
-        this.id = obj.id;
-        this.username = obj.username;
-        this.password = obj.password;
-    }
+class User {
+  constructor(obj) {
+    this.id = obj.id;
+    this.username = obj.username;
+    this.password = obj.password;
+    this.tickets = [];
+  }
 }
 
+class Event {
+  constructor(obj) {
+    this.id = obj.id;
+    this.eventname = obj.eventname;
+    this.location = obj.location;
+    this.category = obj.category;
+    this.poster = obj.poster;
+    this.description = obj.description;
+    this.date = obj.date;
+    this.totalticketavailable = obj.totalticketavailable;
+    this.ticketprice = obj.ticketprice;
+    this.tickets = [];
+  }
+}
