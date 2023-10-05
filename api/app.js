42126ad3f86c1b6f96ed2ffb845a79d2de5449c2
codingvaleria <codingvaleria@gmail.com>
2023-10-05 16:15:20 +0300

    Set up server

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
-// const express = require("express");
+const express = require("express");
+const app = express();
 
-// const app = express();
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It Works!'
+    });
+});
 
-// app.use(express.json());
-
-// module.exports = app;
+module.exports = app;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
+const http = require("http");
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const server = http.createServer(app);
+
+server.listen(port);
