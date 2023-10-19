80a2a55b4e7478fde0d94b84becafcad8d404355
codingvaleria <codingvaleria@gmail.com>
2023-10-19 08:51:50 +0300

    Enabling body parsing

--- api/api/routes/users.js
+++ api/api/routes/users.js
@@ -11,8 +11,8 @@ router.get("/:userId", (req, res, next) => {
   const id = req.params.userId;
   if (id === "special") {
     res.status(200).json({
-        message: "You disovered the special ID",
-        id: id
+      message: "You disovered the special ID",
+      id: id,
     });
   } else {
     res.status(200).json({
@@ -22,8 +22,13 @@ router.get("/:userId", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+  const user = {
+    username: req.body.username,
+  };
+
   res.status(201).json({
     message: "Users were created",
+    createdUser: user,
   });
 });
 
--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const userRoutes = require("./api/routes/users");
+
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/users", userRoutes);
 
